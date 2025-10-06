35758996e53139e96a4aaef7b2612efb84c8c271
Rockrem984 <d24390246@huauchinango.tecnm.mx>
2025-10-06 19:25:36 +0000

    modificacion

--- simplex.py
+++ simplex.py
@@ -1,27 +1,27 @@
-import numpy as np 
-ESP=1e-9
-def pivot (table, row,col):
-    table[row, :]=table[row, :]/table[row,col]
-    m,n=table.shape
+import numpy as np
+EPS = 1e-9
+def pivot(table, row, col):
+    table[row, :] = table[row, :] / table[row, col]
+    m, n = table.shape
     for r in range(m):
-        if r !=row:
-            table[r, :]-=table[r, col]*table[row, :]
-
+        if r != row:
+            table[r, :] -= table[r, col] * table[row, :]
+#_
 def find_entering_col(table):
-    last = table.shape[0]-1
+    last = table.shape[0] - 1
     row = table[last, :-1]
     min_val = np.min(row)
-    if min_val <-EPS:
+    if min_val < -EPS:
         return int(np.argmin(row))
     return None
 
 def find_leaving_row(table, col):
-    m = table.shape[0]-1
-    ratios =[]
+    m = table.shape[0] - 1
+    ratios = []
     for i in range(m):
-        a = table[i, col] 
+        a = table[i, col]
         if a > EPS:
-            ratios.append(table[i,-1]/a)
+            ratios.append(table[i, -1] / a)
         else:
             ratios.append(np.inf)
     ratios = np.array(ratios)
@@ -33,51 +33,48 @@ def simplex_max(table):
     while True:
         col = find_entering_col(table)
         if col is None:
-            return , table
+            return "optimal", table
         row = find_leaving_row(table, col)
         if row is None:
-            return ,table
-        pivot(table, row, col)        
+            return "unbounded", table
+        pivot(table, row, col)
 
-def extract_solution(table, num_decision_vars, basis_col ):
-    m = table.shape[0]-1
+def extract_solution(table, num_decision_vars, basis_cols):
+    m = table.shape[0] - 1
     solution = np.zeros(num_decision_vars)
-    for j in range (num_decision_vars):
-        col = table[:m,j]
-        if np.count_nonzero(np.abs(col - 1.0)<EPS) == 1 and np.count_nonzero(np.abs(col)> EPS) == 1:
-            i = int (np.where(np.abs(col -1.0)<EPS)[0][0])
-            solution[j] = table[i,-1]
+    for j in range(num_decision_vars):
+        col = table[:m, j]
+        if np.count_nonzero(np.abs(col - 1.0) < EPS) == 1 and np.count_nonzero(np.abs(col) > EPS) == 1:
+            i = int(np.where(np.abs(col - 1.0) < EPS)[0][0])
+            solution[j] = table[i, -1]
         else:
             solution[j] = 0.0
-    objective_value = table[-1,-1]
+    objective_value = table[-1, -1]
     return solution, objective_value
 
-def build_initial_table(n, m, A, b, relation, c, maximize=True):
-    A=np.array(A, dtype=float)
-    b=np.array(b, dtype=float)
-    c=np.array(c, dtype=float)
+def build_initial_table(n, m, A, b, relations, c, maximize=True):
+    A = np.array(A, dtype=float)
+    b = np.array(b, dtype=float)
+    c = np.array(c, dtype=float)
 
     slack_cols = []
     art_cols = []
     basis = [-1] * m
-
-    col = n
-
+    cols = n
     slack_count = 0
     art_count = 0
-
     for i in range(m):
-        if relations[i] == '<=' :
+        if relations[i] == '<=':
             slack_count += 1
         elif relations[i] == '>=':
-            slack_count += 1
+            slack_count += 1  
             art_count += 1
         elif relations[i] == '=':
-            art_count +=1
-            
-    total_cols = n + slack_count + art_count + 1
-    table = np.zeros((m+1,total_cols))
-   
+            art_count += 1
+
+    total_cols = n + slack_count + art_count + 1  
+    table = np.zeros((m + 1, total_cols))
+
     table[:m, :n] = A
     col_ptr = n
     slack_ptr = []
@@ -91,7 +88,7 @@ def build_initial_table(n, m, A, b, relation, c, maximize=True):
             basis[i] = col_ptr
             col_ptr += 1
         elif rel == '>=':
-             table[i, col_ptr] = -1.0
+            table[i, col_ptr] = -1.0
             slack_ptr.append(col_ptr)
             col_ptr += 1
             table[i, col_ptr] = 1.0
@@ -110,37 +107,6 @@ def build_initial_table(n, m, A, b, relation, c, maximize=True):
     if maximize:
         table[-1, :n] = -c
     else:
-        table[-1, :n] = c 
+        table[-1, :n] = c  
 
     return table, slack_ptr, art_ptr, basis
-
-def make_phase1_objective(table, art_cols):
-    m, n = table.shape
-    for col in art_cols:
-        table[-1, col] = -1.0
-    table[-1, -1] = 0.0
-
-    for col in art_cols:
-        rows_with_one = np.where(np.abs(table[:m-1, col] - 1.0) < EPS)[0]
-        if rows_with_one.size == 1:
-            i = int(rows_with_one[0])
-            table[-1, :] -= table[i, :]
-
-def remove_artificial_columns(table, art_cols):
-     keep_cols = [j for j in range(table.shape[1]-1) if j not in art_cols] + [-1]
-    new_table = table[:, keep_cols]
-    return new_table
-
-def run_two_phase(n, m, A, b, relations, c, maximize=True):
-    table, slack_cols, art_cols, basis = build_initial_table(n, m, A, b, relations, c, maximize=True)
-    art_cols = list(art_cols)
-    decision_range = list(range(n))
-
-    if art_cols:
-        make_phase1_objective(table, art_cols)
-        status, table = simplex_max(table)
-        phase1_value = table[-1, -1]
-    if phase1_value < -EPS:    
-    
-               
-
